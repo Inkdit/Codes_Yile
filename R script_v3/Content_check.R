54577c29f04aecd15dded224722c4dbb96e37943
Yile Zhang <Yile Zhang>
2016-10-12 16:34:15 -0600

    Directly fetch the contract content from database and strip all the useless tags

---
+++ R script_v3/Content_check.R
@@ -0,0 +1,23 @@
+rm(list = ls())
+library(RPostgreSQL)
+
+libs <- c('tm','wordcloud','Rgraphviz')
+lapply(libs,require, character.only = TRUE)
+
+drv <- dbDriver("PostgreSQL")
+conn <- dbConnect(drv, dbname = "Inkdit", user = "postgres", password = "314159")
+result <- dbGetQuery(conn, "SELECT content FROM contracts WHERE creator_id = 61179")
+result = t(result)
+
+cleanFun <- function(htmlString) {
+  htmlString<-(gsub("<.*?>", "", htmlString))
+  htmlString<-(gsub("&#+", "", htmlString))
+  htmlString<-(gsub("x[A-Z]+", "", htmlString))
+  htmlString<-(gsub("x[0-9]+", "", htmlString))
+  
+  return(gsub("\n", "", htmlString))
+  
+}
+
+kankan <- cleanFun(result)
+View(kankan[20])
