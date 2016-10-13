9da0a2b5be97f0109c037a0cff318b0917078348
Yile Zhang <Yile Zhang>
2016-10-13 15:52:22 -0600

    The function to find most frequent-using user

--- R script_v3/User_Frequency.R
+++ R script_v3/User_Frequency.R
@@ -1,11 +1,17 @@
-rm(list = ls())
+FrequentUser <- function(Freq){
 library(RPostgreSQL)
 drv <- dbDriver("PostgreSQL")
 conn <- dbConnect(drv, dbname = "Inkdit", user = "postgres", password = "314159")
 result <- dbGetQuery(conn, "SELECT * FROM (select creator_id, count(*) AS totalNum from contracts group by creator_id) AS foo ORDER BY totalNum")
-median(t(result[2]))
-max(t(result[2]))
+
+dim(result[2])
+max(result[2])
 aa = quantile(t(result[2]), probs = seq(0, 1, by= 0.01))
+
+plot(1:nrow(result), cumsum(t(result[2]))/sum(t(result[2])))
+aa
 barplot(aa)
 
-
+Frequent_user = subset(result, result[2] >= Freq)
+return(Frequent_user)
+}
